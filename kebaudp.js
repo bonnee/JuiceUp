903b1d166a2f0c79e2e70e7b1c28e96726a73f53
Matteo Bonora <bonora.matteo@gmail.com>
2018-09-18 00:44:19 +0200

    Clean code

--- kebaudp.js
+++ kebaudp.js
@@ -1,71 +1,73 @@
 const dgram = require('dgram');
 const PORT = 7090;
-const BRD_PORT = 7092
-const POLL_INTERVAL = 30000;
+const BRD_PORT = 7092;
+
+const POLL_FREQ = 30000;
+const REQ_FREQ = 10;
 
 class KeContact {
 	constructor(address) {
-		this.socket = dgram.createSocket('udp4');
-		this.server = dgram.createSocket('udp4');
-		this.broadServer = dgram.createSocket('udp4');
+		this.txSocket = dgram.createSocket('udp4');
+		this.rxSocket = dgram.createSocket('udp4');
+		this.brdSocket = dgram.createSocket('udp4');
 
 		this.address = address;
 
 		this.sendQueue = [];
+
 		this.data = {};
 		this.history = [];
 
 		this.timer;
 
-		this.server.on('error', (err) => {
+		this.rxSocket.on('error', (err) => {
 			console.error(err);
 			process.exit(1);
 		});
 
-		this.broadServer.on('message', (message, remote) => {
-			adapter.log.debug('UDP broadcast datagram from ' + remote.address + ':' + remote.port + ': "' + message + '"');
+		this.brdSocket.on('message', (message, remote) => {
 			try {
-				this.resetTimer();
-				this.requestReports();
+				this._resetTimer();
+				this._updateHistory();
 
-				this.parseMessage(message);
+				this._saveData(this._parseMessage(message));
 			} catch (e) {
-				adapter.log.warn('Error handling message: ' + e);
+				console.log('Error handling message: ' + e);
 			}
 		});
 
-		this.broadServer.on('listening', () => {
+		this.brdSocket.on('listening', () => {
 			console.log('Broadcast server listening');
 
-			this.broadServer.setBroadcast(true);
-			this.broadServer.setMulticastLoopback(true);
+			this.brdSocket.setBroadcast(true);
+			this.brdSocket.setMulticastLoopback(true);
 		});
 
-		this.server.on('listening', () => {
+		this.rxSocket.on('listening', () => {
 			console.log('Server listening');
 
-			this.send('report 1');
-			this.updateReports();
-			this.updateHistory();
+			this._send('report 1');
+			this._updateReports();
+			this._updateHistory();
 
-			this.resetTimer();
+			this._resetTimer();
 		});
 
-		this.server.bind(PORT);
-		this.broadServer.bind(BRD_PORT, '0.0.0.0');
+		this.rxSocket.bind(PORT);
+		this.brdSocket.bind(BRD_PORT, '0.0.0.0');
 	}
 
-	resetTimer() {
+	_resetTimer() {
 		if (this.timer)
 			clearInterval(this.timer);
 
 		this.timer = setInterval(() => {
-			console.log('Updating data...')
-			this.updateReports();
-		}, POLL_INTERVAL);
+			console.log('Update data');
+			this._updateReports();
+		}, POLL_FREQ);
 	}
 
-	parseMessage(message) {
+	_parseMessage(message) {
 		try {
 			let msg = message.toString().trim();
 
@@ -73,8 +75,8 @@ class KeContact {
 				return;
 
 			if (msg.startsWith('TCH-OK')) {
-				this.resetTimer();
-				this.updateReports();
+				this._resetTimer();
+				this._updateReports();
 				return;
 			}
 
@@ -87,60 +89,57 @@ class KeContact {
 		}
 	}
 
-	saveData(newData) {
+	_saveData(newData) {
 		for (let key in newData) {
 			if (key != 'ID')
 				this.data[key] = newData[key];
 		}
 	}
-	saveHistory(newHistory) {
+	_saveHistory(newHistory) {
 		let id = newHistory.ID;
 
 		this.history[id] = newHistory;
 	}
 
 
-	send(sendMsg) {
+	_send(sendMsg) {
 		this.sendQueue.push(sendMsg);
 
 		if (this.sendQueue.length == 1)
-			this.handleQueue();
+			this._handleQueue();
 	}
 
-	handleQueue() {
+	_handleQueue() {
 		if (this.sendQueue.length == 0)
 			return;
 
 		let sendMsg = this.sendQueue[0];
-		console.log('Sending: ' + sendMsg);
-		this.socket.send(Buffer.from(sendMsg), PORT, this.address, (err) => {
+		this.txSocket.send(Buffer.from(sendMsg), PORT, this.address, (err) => {
 			if (err)
 				console.error(err);
 
 		});
-		this.server.once('message', (message, rinfo) => {
-			console.log('Received response to ' + sendMsg);
-
-			let parsedMessage = this.parseMessage(message);
+		this.rxSocket.once('message', (message, rinfo) => {
+			let parsedMessage = this._parseMessage(message);
 
 			if (parsedMessage.ID >= 10)
-				this.saveHistory(parsedMessage);
+				this._saveHistory(parsedMessage);
 			else
-				this.saveData(parsedMessage);
+				this._saveData(parsedMessage);
 
 			this.sendQueue.shift();
-			this.handleQueue();
+			setTimeout(this._handleQueue.bind(this), REQ_FREQ);
 		});
 	}
 
-	updateReports() {
-		this.send('report 2');
-		this.send('report 3');
+	_updateReports() {
+		this._send('report 2');
+		this._send('report 3');
 	}
 
-	updateHistory() {
+	_updateHistory() {
 		for (let i = 101; i < 131; i++) {
-			this.send('report ' + i)
+			this._send('report ' + i)
 		}
 	}
 
