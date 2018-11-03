a77427ba16541bdd1aca18a159ed2d3b3ae51e5c
Bonnee <bonora.matteo@gmail.com>
2018-11-03 20:24:13 +0100

    Add www

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
 global.__basedir = __dirname;
 
-const WEBPORT = 3000;
-
+const path = require('path');
 const Connections = require(__basedir + '/controllers/connections.js');
 const Kecontact = require(__basedir + '/controllers/kecontact');
 const db = require(__basedir + '/controllers/db.js');
-const express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
+var express = require('express');
+
+var app = express();
 var connections = new Connections();
 
-app.set('views', __basedir + '/views');
+app.set('views', path.join(__basedir, '/views'));
 app.set('view engine', 'pug');
-app.use(express.static(__basedir + '/public'));
+app.use(express.static(path.join(__basedir, '/public')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
-
-app.listen(WEBPORT, function () {
-	console.log('JuiceUp online on port ' + WEBPORT);
-});
 
 db.getWallboxes().forEach(box => {
 	let conn = new Kecontact(box.address);
@@ -35,5 +31,7 @@ db.getWallboxes().forEach(box => {
 
 app.set('connections', connections);
 
-app.use('/', require(__basedir + '/routes/ui.js'));
-app.use('/api', require(__basedir + '/routes/api/index.js'));
+app.use('/', require(path.join(__basedir, '/routes/ui.js')));
+app.use('/api', require(path.join(__basedir, '/routes/api/index.js')));
+
+module.exports = app;
