abae17aa7077ba62f6d7f1e9904328c4f10580fd
Lukas Bisdorf <lbisdorf@vwd.com>
2020-02-27 22:27:30 +0100

    Adds i18n support and included german translation.

--- app.js
+++ app.js
@@ -5,20 +5,34 @@ const Kecontact = require(__basedir + '/controllers/kecontact');
 const db = require(__basedir + '/controllers/db.js');
 const bodyParser = require('body-parser');
 const express = require('express');
+const i18n = require('i18n');
+const cookieParser = require('cookie-parser');
 
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+
+i18n.configure({
+	locales:['en', 'de'],
+	directory: __dirname + '/locales',
+	defaultLocale: 'en',
+	queryParameter: 'lang',
+	register: global,
+	cookie: 'language',
+    objectNotation: true,
+});
+
 app.set('views', path.join(__basedir, '/views'));
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__basedir, '/public')));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
 
+app.use(cookieParser());
+app.use(i18n.init);
 app.locals.moment = require('moment');
 
 io.on('connection', socket => {
@@ -32,7 +46,7 @@ io.on('connection', socket => {
 
 Kecontact.on('message', data => {
 	io.emit(data.serial, data.data);
-})
+});
 
 var connect = (box, ttl) => {
 	return new Promise((resolve, reject) => {
@@ -65,7 +79,13 @@ db.getAllWallboxes().forEach(box => {
 	});
 });
 
-
+app.use(function(req, res, next) {
+	// express helper for natively supported engines
+	res.locals.__ = res.__ = function() {
+		return i18n.__.apply(req, arguments);
+	};
+	next();
+});
 app.use('/', require(path.join(__basedir, '/routes/ui.js')));
 app.use('/api', require(path.join(__basedir, '/routes/api/index.js')));
 
